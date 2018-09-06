45ce2ccf43db8c929ddd5805474c1a8d47dbd788
matrixb0ss <rinthesoft@gmail.com>
2018-09-06 19:25:43 +0300

    SHOW IS ALIVE!

--- src/providers/customDataProvider.js
+++ src/providers/customDataProvider.js
@@ -1,5 +1,5 @@
 import { stringify } from 'query-string';
-import { GET_LIST } from 'react-admin';
+import { GET_LIST, GET_ONE } from 'react-admin';
 
 const API_URL = `https://api.insideview.com/api/v1`;
 const accessToken = localStorage.getItem('token');
@@ -28,13 +28,19 @@ const getQuery = (resource, params) => {
 const transformData = (json, resource) => {
     switch (resource) {
         case 'companies': {
-            json.companies.forEach(c => c.id = c.companyId);
-            return json.companies;
+            if(json.companies) {
+                json.companies.forEach(c => c.id = c.companyId);
+                return json.companies;
+            }
+            return {...json, id: json.companyId}
             break;
         }
         case 'contacts': {
-            json.contacts.forEach(c => c.id = c.contactId);
-            return json.contacts;
+            if(json.contacts) {
+                json.contacts.forEach(c => c.id = c.contactId);
+                return json.contacts;
+            }
+            return {...json, id: json.contactId}
             break;
         }
         default:
@@ -48,12 +54,14 @@ const fetchData = (type, resource, url, options, params) => {
     // let headers;
     return fetch(url, options)
         .then(res => {
+            console.log(res, '------------------------');
             // res.headers.append( 'Access-Control-Expose-Headers', 'Content-Range')
             // res.headers.append('Content-Range','bytes : 0-9/*')
             // headers = res.headers;
             return res.json();
         })
         .then(json => {
+            console.log(json, '!!!!!!!!!!');
             const result = transformData(json, resource);
             console.log(result, '<<<');
             switch (type) {
@@ -61,24 +69,34 @@ const fetchData = (type, resource, url, options, params) => {
                     return {
                         data: result,
                         total: 50,
-                        page: 1,
-                        pagination: 1,
+                    };
+                    break;
+                }
+                case GET_ONE: {
+                    return {
+                        data: result,
+                        total: 50,
                     };
                     break;
                 }
                 default:
                     return { data: json.companies };
             }
-        });
+        })
+        .catch(err => console.log(err || err.message, 'ERROR'))
 }
 
 const getURL = (type, resource, params) => {
-    const query = getQuery(resource, params);
-
+    
     switch (type) {
         case GET_LIST: {
+            const query = getQuery(resource, params);
+            console.log(type, 'GET_lissrsrsrsrs');
             return `${API_URL}/${resource}?${stringify(query)}`;
-            break;
+        }
+        case GET_ONE: {
+            console.log(type, '555555555');
+            return `${API_URL}/company/${params.id}`;
         }
         default:
             throw new Error(`Unsupported Data Provider request type ${type}`);
@@ -86,8 +104,10 @@ const getURL = (type, resource, params) => {
 }
 
 export default (type, resource, params) => {
+    console.log(type, 'TYPE');
     console.log(params, 'params');
     const url = getURL(type, resource, params);
+    console.log(url, 'URL<<<<<<<<<<');
 
     return fetchData(type, resource, url, options, params);
 
