90714e887f626b295f932107ee017d65c48a4f7e
matrixb0ss <rinthesoft@gmail.com>
2018-09-12 13:18:37 +0300

    Delete Dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 
-import Dashboard from './components/Dashboard';
 import { CompanyList } from './components/companyList';
 import { CompanyShow } from './components/companyShow';
 import { ContactList } from './components/contactsList';
@@ -11,7 +10,7 @@ import authProvider from './providers/authProvider';
 import customDataProvider from './providers/customDataProvider';
 
 const App = () => (
-    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={customDataProvider} >
+    <Admin authProvider={authProvider} dataProvider={customDataProvider} >
         <Resource name="companies" list={CompanyList} show={CompanyShow} />
         <Resource name="contacts" list={ContactList} show={ContactShow} />
     </Admin>
--- src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-
-export default () => (
-    <Card>
-        <CardHeader title="Welcome back!" />
-    </Card>
-);
