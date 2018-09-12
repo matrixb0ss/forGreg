03fe71a9047864dd1e55b5b73347c4d6157110f6
matrixb0ss <rinthesoft@gmail.com>
2018-09-12 13:47:30 +0300

    Display all company details once the user clicks on show

--- src/components/companyShow.js
+++ src/components/companyShow.js
@@ -7,14 +7,35 @@ export const CompanyShow = (props) => (
             <TextField source="city" />
             <TextField source="companyBlogProfile" />
             <TextField source="companyFacebookProfile" />
-            <TextField source="state" />
+            <TextField source="companyId" />
+            <TextField source="companyLinkedInProfile" />
             <TextField source="companyStatus" />
+            <TextField source="companyTaxId" />
+            <TextField source="companyTwitterProfile" />
+            <TextField source="companyType" />
             <TextField source="country" />
+            <TextField source="employees" />
+            <TextField source="equifaxId" />
             <TextField source="fax" />
+            <TextField source="foundationDate" />
             <TextField source="industry" />
+            <TextField source="industryCode" />
+            <TextField source="latitude" />
+            <TextField source="longitude" />
+            <TextField source="naics" />
+            <TextField source="naicsDescription" />
             <TextField source="name" />
             <TextField source="phone" />
+            <TextField source="revenue" />
+            <TextField source="revenueCurrency" />
+            <TextField source="sic" />
+            <TextField source="sicDescription" />
+            <TextField source="state" />
             <TextField source="street" />
+            <TextField source="subIndustry" />
+            <TextField source="subIndustryCode" />
+            <TextField source="subsidiary" />
+            <TextField source="websites" />
             <TextField source="zip" />
         </SimpleShowLayout>
     </Show>
