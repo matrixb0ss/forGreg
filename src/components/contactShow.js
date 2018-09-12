96bc54e7faf428b0138444ae7fc2f9037af1411a
matrixb0ss <rinthesoft@gmail.com>
2018-09-12 15:10:22 +0300

    Display all contact details once the user clicks on the show

--- src/components/contactShow.js
+++ src/components/contactShow.js
@@ -5,13 +5,30 @@ export const ContactShow = (props) => (
     <Show {...props}>
         <SimpleShowLayout>
             <TextField source="active" />
+            <TextField source="age" />
+            <TextField source="companyId" />
             <TextField source="companyName" />
             <TextField source="confidenceScore" />
             <TextField source="contactId" />
+            <TextField source="description" />
             <TextField source="email" />
+            <TextField source="education" />
+            <TextField source="facebookHandle" />
+            <TextField source="firstName" />
             <TextField source="fullName" />
-            <TextField source="peopleId" />
+            <TextField source="imageUrl" />
+            <TextField source="jobFunctions" />
+            <TextField source="jobLevels" />
+            <TextField source="lastName" />
+            <TextField source="linkedinHandle" />
             <TextField source="phone" />
+            <TextField source="peopleId" />
+            <TextField source="sources" />
+            <TextField source="salary" />
+            <TextField source="salaryCurrency" />
+            <TextField source="salaryCurrency" />
+            <TextField source="titles" />
+            <TextField source="twitterHandle" />
         </SimpleShowLayout>
     </Show>
 );
