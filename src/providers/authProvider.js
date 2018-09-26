64ea2a7b872abfd68f6b4e98bbdce847a7755bb3
matrixb0ss <rinthesoft@gmail.com>
2018-09-26 16:16:20 +0300

    Change spacing

--- src/providers/authProvider.js
+++ src/providers/authProvider.js
@@ -2,31 +2,31 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
 import { getAccessToken, deleteAccessToken } from './../api'
 
 export default async (type, params) => {
-    if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
+  if (type === AUTH_LOGIN) {
+    const { username } = params;
+    localStorage.setItem('username', username);
 
-        return getAccessToken();
-    }
+    return getAccessToken();
+  }
 
-    if (type === AUTH_LOGOUT) {
-        deleteAccessToken();
-        return Promise.resolve();
-    }
+  if (type === AUTH_LOGOUT) {
+    deleteAccessToken();
+    return Promise.resolve();
+  }
 
-    if (type === AUTH_ERROR) {
-        const { status } = params;
-        if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
-            return Promise.reject();
-        }
-        return Promise.resolve();
+  if (type === AUTH_ERROR) {
+    const { status } = params;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('username');
+      return Promise.reject();
     }
+    return Promise.resolve();
+  }
 
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
-            ? Promise.resolve()
-            : Promise.reject();
-    }
-    return Promise.reject('Unknown method');
+  if (type === AUTH_CHECK) {
+    return localStorage.getItem('username')
+      ? Promise.resolve()
+      : Promise.reject();
+  }
+  return Promise.reject('Unknown method');
 };
--- src/providers/customDataProvider.js
+++ src/providers/customDataProvider.js
@@ -5,100 +5,99 @@ import _ from 'lodash';
 const API_URL = `https://api.insideview.com/api/v1`;
 
 const getQuery = (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const query = {
-        sort: JSON.stringify([field, order]),
-        range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-        page: `${page}`,
-    };
-    const filterIsEmpty = _.isEmpty(params.filter)
+  const { page, perPage } = params.pagination;
+  const { field, order } = params.sort;
+  const query = {
+    sort: JSON.stringify([field, order]),
+    range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+    page: `${page}`,
+  };
+  const filterIsEmpty = _.isEmpty(params.filter)
 
-    if (resource === 'companies') {
-        query.name = filterIsEmpty ? 's' : JSON.stringify(params.filter.search);
-    }
+  if (resource === 'companies') {
+    query.name = filterIsEmpty ? 's' : JSON.stringify(params.filter.search);
+  }
 
-    if (resource === 'contacts') {
-        query.fullName = filterIsEmpty ? 'G' : JSON.stringify(params.filter.search);
-    }
-    return query;
+  if (resource === 'contacts') {
+    query.fullName = filterIsEmpty ? 'G' : JSON.stringify(params.filter.search);
+  }
+  return query;
 }
 
 const getURL = (type, resource, params) => {
-    switch (type) {
-        case GET_LIST: {
-            const query = getQuery(resource, params);
-            return `${API_URL}/${resource}?${stringify(query)}`;
-        }
-        case GET_ONE: {
-            let single = '';
-            if (resource === 'companies') single = 'company'
-            if (resource === 'contacts') single = 'contact'
-            return `${API_URL}/${single}/${params.id}`;
-        }
-        default:
-            throw new Error(`Unsupported Data Provider request type ${type}`);
+  switch (type) {
+    case GET_LIST: {
+      const query = getQuery(resource, params);
+      return `${API_URL}/${resource}?${stringify(query)}`;
     }
+    case GET_ONE: {
+      let single = '';
+      if (resource === 'companies') single = 'company'
+      if (resource === 'contacts') single = 'contact'
+      return `${API_URL}/${single}/${params.id}`;
+    }
+    default:
+      throw new Error(`Unsupported Data Provider request type ${type}`);
+  }
 }
 
 const transformData = (json, resource) => {
-    switch (resource) {
-        case 'companies': {
-            if (json.companies) {
-                json.companies.forEach(c => c.id = c.companyId);
-                return json.companies;
-            }
-            return {...json, id: json.companyId}
-        }
-        case 'contacts': {
-            if(json.contacts) {
-                json.contacts.forEach(c => c.id = c.contactId);
-                return json.contacts;
-            }
-            return {...json, id: json.contactId}
-        }
-        default:
-            return json.data;
+  switch (resource) {
+    case 'companies': {
+      if (json.companies) {
+          json.companies.forEach(c => c.id = c.companyId);
+          return json.companies;
+      }
+      return {...json, id: json.companyId}
     }
+    case 'contacts': {
+      if(json.contacts) {
+          json.contacts.forEach(c => c.id = c.contactId);
+          return json.contacts;
+      }
+      return {...json, id: json.contactId}
+    }
+    default:
+      return json.data;
+  }
 }
 
 const fetchData = (type, resource, url) => {
-    const accessToken = localStorage.getItem('token');
-    const options = { 
-        headers : new Headers({
-            Accept: 'application/json',
-            accessToken,
-        }),
-    };
-    return fetch(url, options)
-        .then(res => {
-            console.log(res);
-            if (res.status !== 200) return { data: [] };
-            return res.json();
-        })
-        .then(json => {
-            const result = transformData(json, resource);
-            const totalResults = (json.totalResults < 100000) ? json.totalResults : 100000;  
-            switch (type) {
-                case GET_LIST: {
-                    return {
-                        data: result,
-                        total: totalResults,
-                    };
-                }
-                case GET_ONE: {
-                    return {
-                        data: result,
-                    };
-                }
-                default:
-                    return { data: json };
-            }
-        })
-        .catch(err => console.warn(err || err.message));
+  const accessToken = localStorage.getItem('token');
+  const options = {
+    headers : new Headers({
+      Accept: 'application/json',
+      accessToken,
+    }),
+  };
+  return fetch(url, options)
+    .then(res => {
+      if (res.status !== 200) return { data: [] };
+      return res.json();
+    })
+    .then(json => {
+      const result = transformData(json, resource);
+      const totalResults = (json.totalResults < 100000) ? json.totalResults : 100000;
+      switch (type) {
+        case GET_LIST: {
+          return {
+            data: result,
+            total: totalResults,
+          };
+        }
+        case GET_ONE: {
+          return {
+            data: result,
+          };
+        }
+        default:
+          return { data: json };
+      }
+    })
+    .catch(err => console.warn(err || err.message));
 }
 
 export default (type, resource, params) => {
-    const url = getURL(type, resource, params);
-    return fetchData(type, resource, url, params);  
+  const url = getURL(type, resource, params);
+  return fetchData(type, resource, url, params);
 };
